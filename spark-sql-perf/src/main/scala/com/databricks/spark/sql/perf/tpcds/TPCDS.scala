069148516c4af73f199fe486423718a65e549683
ngtrkhoa <ngtrkhoa@gmail.com>
2016-04-05 15:25:50 +0200

    Re-add project spark-sql-perf

---
+++ spark-sql-perf/src/main/scala/com/databricks/spark/sql/perf/tpcds/TPCDS.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2015 Databricks Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.databricks.spark.sql.perf.tpcds
+
+import com.databricks.spark.sql.perf._
+import org.apache.spark.sql.SQLContext
+
+/**
+ * TPC-DS benchmark's dataset.
+ * @param sqlContext An existing SQLContext.
+ */
+class TPCDS (
+    @transient sqlContext: SQLContext,
+    resultsLocation: String = "/spark/sql/performance",
+    resultsTableName: String = "sqlPerformance")
+  extends Benchmark(sqlContext, resultsLocation, resultsTableName)
+  with ImpalaKitQueries with SimpleQueries with Serializable {
+
+  /*
+  def setupBroadcast(skipTables: Seq[String] = Seq("store_sales", "customer")) = {
+    val skipExpr = skipTables.map(t => !('tableName === t)).reduceLeft[Column](_ && _)
+    val threshold =
+      allStats
+        .where(skipExpr)
+        .select(max('sizeInBytes))
+        .first()
+        .getLong(0)
+    val setQuery = s"SET spark.sql.autoBroadcastJoinThreshold=$threshold"
+
+    println(setQuery)
+    sql(setQuery)
+  }
+  */
+}
+
