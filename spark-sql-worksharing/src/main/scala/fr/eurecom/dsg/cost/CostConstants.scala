83c402be6c45f05c835267e7fd1e37be5b89acd2
ngtrkhoa <ngtrkhoa@gmail.com>
2016-09-21 23:24:18 +0200

    adjust costs

--- spark-sql-worksharing/src/main/scala/fr/eurecom/dsg/cost/CostConstants.scala
+++ spark-sql-worksharing/src/main/scala/fr/eurecom/dsg/cost/CostConstants.scala
@@ -2,10 +2,10 @@ package fr.eurecom.dsg.cost
 
 object CostConstants {
   val COST_DISK_READ:Double = 0.1f // care disk cache of OS
-  val COST_NETWORK:Double = 5f // should be expensive
-  val COST_SIMPLE_OP:Double = 0.00001f // should be small
+  val COST_NETWORK:Double = 10f // should be expensive
+  val COST_SIMPLE_OP:Double = 1e-6f // should be small // filter, project
 
-  val COST_RAM_READ:Double = 0.001f // RAM READ = 100x DISK READ
+  val COST_RAM_READ:Double = 1e-3f // RAM READ = 100x DISK READ
   val COST_RAM_WRITE = 0.1f
 
   val DEFAULT_SELECTIVITY:Double = 0.33f
--- spark-sql-worksharing/src/main/scala/test.scala
+++ spark-sql-worksharing/src/main/scala/test.scala
@@ -10,18 +10,19 @@ import scala.concurrent.Await
 
 object test {
   def main(args: Array[String]) {
-    val mailer = Mailer("smtp.gmail.com", 25)
-      .auth(true)
-      .as("khoa.mailer@gmail.com", "abcxyz1234")
-      .startTtls(true)()
-
-    val f =mailer(Envelope.from(new InternetAddress("khoa.mailer@gmail.com"))
-      .to(new InternetAddress("ngtrkhoa@gmail.com"))
-      .subject("Job done")
-      .content(Text("pls check ui")))
-
-    val x = Await.ready(f, scala.concurrent.duration.Duration.Inf).value.get
-    println(x)
+    println((1e-2))
+//    val mailer = Mailer("smtp.gmail.com", 25)
+//      .auth(true)
+//      .as("khoa.mailer@gmail.com", "abcxyz1234")
+//      .startTtls(true)()
+//
+//    val f =mailer(Envelope.from(new InternetAddress("khoa.mailer@gmail.com"))
+//      .to(new InternetAddress("ngtrkhoa@gmail.com"))
+//      .subject("Job done")
+//      .content(Text("pls check ui")))
+//
+//    val x = Await.ready(f, scala.concurrent.duration.Duration.Inf).value.get
+//    println(x)
   }
 
 }
