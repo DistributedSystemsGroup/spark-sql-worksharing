030422efe6226e29ab49ad3c02acfdecf150a180
ngtrkhoa <ngtrkhoa@gmail.com>
2016-10-10 23:34:32 +0200

    add comments

--- spark-sql-worksharing/src/main/scala/fr/eurecom/dsg/applications/microbenchmark/DataRow.scala
+++ spark-sql-worksharing/src/main/scala/fr/eurecom/dsg/applications/microbenchmark/DataRow.scala
@@ -1,33 +1,34 @@
 package fr.eurecom.dsg.applications.microbenchmark
 
 import org.apache.spark.sql.types._
-
 import scala.collection.mutable.ArrayBuffer
 
-/**
-  * Created by ntkhoa on 30/06/16.
+/** Schema for the base table in the micro benchmark
+  * n1-n10: int
+  * d1-d10: double
+  * s1-s10: string
   */
-class DataRow(val n1:Int, val n2:Int, val n3:Int, val n4:Int, val n5:Int, val n6:Int, val n7:Int, val n8:Int, val n9:Int, val n10:Int,
-              val d1:Double, val d2:Double, val d3:Double, val d4:Double, val d5:Double, val d6:Double, val d7:Double, val d8:Double, val d9:Double, val d10:Double,
-              val s1:String, val s2:String, val s3:String, val s4:String, val s5:String, val s6:String, val s7:String, val s8:String, val s9:String, val s10:String) extends Product{
+class DataRow(val n1: Int, val n2: Int, val n3: Int, val n4: Int, val n5: Int, val n6: Int, val n7: Int, val n8: Int, val n9: Int, val n10: Int,
+              val d1: Double, val d2: Double, val d3: Double, val d4: Double, val d5: Double, val d6: Double, val d7: Double, val d8: Double, val d9: Double, val d10: Double,
+              val s1: String, val s2: String, val s3: String, val s4: String, val s5: String, val s6: String, val s7: String, val s8: String, val s9: String, val s10: String) extends Product {
 
-  def this(text:Array[String]){
-    this(text(0).toInt, text(1).toInt, text(2).toInt, text(3).toInt, text(4).toInt, text(5).toInt, text(6).toInt, text(7).toInt, text(8).toInt, text(9).toInt,
-      text(10).toDouble, text(11).toDouble, text(12).toDouble, text(13).toDouble, text(14).toDouble, text(15).toDouble, text(16).toDouble, text(17).toDouble, text(18).toDouble, text(19).toDouble,
-      text(20), text(21), text(22), text(23), text(24), text(25), text(26), text(27), text(28), text(29))
+  def this(fieldValues: Array[String]) {
+    this(fieldValues(0).toInt, fieldValues(1).toInt, fieldValues(2).toInt, fieldValues(3).toInt, fieldValues(4).toInt, fieldValues(5).toInt, fieldValues(6).toInt, fieldValues(7).toInt, fieldValues(8).toInt, fieldValues(9).toInt,
+      fieldValues(10).toDouble, fieldValues(11).toDouble, fieldValues(12).toDouble, fieldValues(13).toDouble, fieldValues(14).toDouble, fieldValues(15).toDouble, fieldValues(16).toDouble, fieldValues(17).toDouble, fieldValues(18).toDouble, fieldValues(19).toDouble,
+      fieldValues(20), fieldValues(21), fieldValues(22), fieldValues(23), fieldValues(24), fieldValues(25), fieldValues(26), fieldValues(27), fieldValues(28), fieldValues(29))
   }
 
-  override def productElement(n: Int): Any = n match{
-    case 0=> n1
-    case 1=> n2
-    case 2=> n3
-    case 3=> n4
-    case 4=> n5
-    case 5=> n6
-    case 6=> n7
-    case 7=> n8
-    case 8=> n9
-    case 9=> n10
+  override def productElement(n: Int): Any = n match {
+    case 0 => n1
+    case 1 => n2
+    case 2 => n3
+    case 3 => n4
+    case 4 => n5
+    case 5 => n6
+    case 6 => n7
+    case 7 => n8
+    case 8 => n9
+    case 9 => n10
 
     case 10 => d1
     case 11 => d2
@@ -59,17 +60,17 @@ class DataRow(val n1:Int, val n2:Int, val n3:Int, val n4:Int, val n5:Int, val n6
   override def canEqual(that: Any): Boolean = that.isInstanceOf[DataRow]
 }
 
-object DataRow{
-  def getSchema():StructType = {
+object DataRow {
+  def getSchema(): StructType = {
     val fields = new ArrayBuffer[StructField]()
-    for(i <- 1 until 11){
-      fields.append(StructField("n" + i, IntegerType, false))
+    for (i <- 1 until 11) {
+      fields.append(StructField("n" + i, IntegerType, nullable = false))
     }
-    for(i <- 1 until 11){
-      fields.append(StructField("d" + i, DoubleType, false))
+    for (i <- 1 until 11) {
+      fields.append(StructField("d" + i, DoubleType, nullable = false))
     }
-    for(i <- 1 until 11){
-      fields.append(StructField("s" + i, StringType, false))
+    for (i <- 1 until 11) {
+      fields.append(StructField("s" + i, StringType, nullable = false))
     }
 
     StructType(fields.toArray)
--- spark-sql-worksharing/src/main/scala/fr/eurecom/dsg/applications/microbenchmark/RefDataRow.scala
+++ spark-sql-worksharing/src/main/scala/fr/eurecom/dsg/applications/microbenchmark/RefDataRow.scala
@@ -1,33 +1,34 @@
 package fr.eurecom.dsg.applications.microbenchmark
 
 import org.apache.spark.sql.types._
-
 import scala.collection.mutable.ArrayBuffer
 
-/**
-  * Created by ntkhoa on 30/06/16.
+/** Schema for the reference table in the micro benchmark
+  * ref_n1-10: int
+  * ref_d1-10: double
+  * ref_s1-10: string
   */
-class RefDataRow(val ref_n1:Int, val ref_n2:Int, val ref_n3:Int, val ref_n4:Int, val ref_n5:Int, val ref_n6:Int, val ref_n7:Int, val ref_n8:Int, val ref_n9:Int, val ref_n10:Int,
-              val ref_d1:Double, val ref_d2:Double, val ref_d3:Double, val ref_d4:Double, val ref_d5:Double, val ref_d6:Double, val ref_d7:Double, val ref_d8:Double, val ref_d9:Double, val ref_d10:Double,
-              val ref_s1:String, val ref_s2:String, val ref_s3:String, val ref_s4:String, val ref_s5:String, val ref_s6:String, val ref_s7:String, val ref_s8:String, val ref_s9:String, val ref_s10:String) extends Product{
+class RefDataRow(val ref_n1: Int, val ref_n2: Int, val ref_n3: Int, val ref_n4: Int, val ref_n5: Int, val ref_n6: Int, val ref_n7: Int, val ref_n8: Int, val ref_n9: Int, val ref_n10: Int,
+                 val ref_d1: Double, val ref_d2: Double, val ref_d3: Double, val ref_d4: Double, val ref_d5: Double, val ref_d6: Double, val ref_d7: Double, val ref_d8: Double, val ref_d9: Double, val ref_d10: Double,
+                 val ref_s1: String, val ref_s2: String, val ref_s3: String, val ref_s4: String, val ref_s5: String, val ref_s6: String, val ref_s7: String, val ref_s8: String, val ref_s9: String, val ref_s10: String) extends Product {
 
-  def this(text:Array[String]){
+  def this(text: Array[String]) {
     this(text(0).toInt, text(1).toInt, text(2).toInt, text(3).toInt, text(4).toInt, text(5).toInt, text(6).toInt, text(7).toInt, text(8).toInt, text(9).toInt,
       text(10).toDouble, text(11).toDouble, text(12).toDouble, text(13).toDouble, text(14).toDouble, text(15).toDouble, text(16).toDouble, text(17).toDouble, text(18).toDouble, text(19).toDouble,
       text(20), text(21), text(22), text(23), text(24), text(25), text(26), text(27), text(28), text(29))
   }
 
-  override def productElement(n: Int): Any = n match{
-    case 0=> ref_n1
-    case 1=> ref_n2
-    case 2=> ref_n3
-    case 3=> ref_n4
-    case 4=> ref_n5
-    case 5=> ref_n6
-    case 6=> ref_n7
-    case 7=> ref_n8
-    case 8=> ref_n9
-    case 9=> ref_n10
+  override def productElement(n: Int): Any = n match {
+    case 0 => ref_n1
+    case 1 => ref_n2
+    case 2 => ref_n3
+    case 3 => ref_n4
+    case 4 => ref_n5
+    case 5 => ref_n6
+    case 6 => ref_n7
+    case 7 => ref_n8
+    case 8 => ref_n9
+    case 9 => ref_n10
 
     case 10 => ref_d1
     case 11 => ref_d2
@@ -59,16 +60,16 @@ class RefDataRow(val ref_n1:Int, val ref_n2:Int, val ref_n3:Int, val ref_n4:Int,
   override def canEqual(that: Any): Boolean = that.isInstanceOf[RefDataRow]
 }
 
-object RefDataRow{
-  def getSchema():StructType = {
+object RefDataRow {
+  def getSchema(): StructType = {
     val fields = new ArrayBuffer[StructField]()
-    for(i <- 1 until 11){
+    for (i <- 1 until 11) {
       fields.append(StructField("ref_n" + i, IntegerType, false))
     }
-    for(i <- 1 until 11){
+    for (i <- 1 until 11) {
       fields.append(StructField("ref_d" + i, DoubleType, false))
     }
-    for(i <- 1 until 11){
+    for (i <- 1 until 11) {
       fields.append(StructField("ref_s" + i, StringType, false))
     }
 
